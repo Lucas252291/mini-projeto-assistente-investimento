5f03c081caec6a4a7ac231dd30db049859d8e543
Lucas252291 <lucas.jlrs252291@gmail.com>
2025-05-24 22:51:57 -0300

    inicialização

---
+++ main.py
@@ -0,0 +1,19 @@
+cor_vermelho = "\033[31m"
+cor_verde = "\033[32m"
+cor_azul = "\033[34m"
+cor_amarelo = "\033[33m"
+cor_roxo = "\033[35m"
+cor_ciano = "\033[36m"
+cor_branco = "\033[37m"
+italico = "\033[3m"
+negativo = "\033[7m"
+reset = "\033[0m"
+
+
+print("SIMULADOR DE INVESTIMENTOS")
+print("Olá, vou te ajudar a simular as possibilidades de investimento")
+
+print(f"Pra começar, quero te dizer que as {cor_azul}taxas anuais{reset} que estou utilizando ")
+
+
+
